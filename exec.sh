d59b80e51f1b3269b6f816339b68b1160d6f6c88
Harsh J <harsh@cloudera.com>
2016-07-21 09:17:18 +0530

    Update execution script to cover parcels

--- exec.sh
+++ exec.sh
@@ -1,9 +1,28 @@
-source /usr/libexec/bigtop-detect-javahome
+source /usr/bin/bigtop-detect-javahome
 export CLASSPATH=/etc/hadoop/conf
-for file in `ls /usr/lib/hadoop/client/*.jar`
+
+BASE_PACKAGE_PATH="/usr/lib/hadoop/"
+BASE_PARCEL_PATH="/opt/cloudera/parcels/CDH/"
+
+CLIENT_DIR="${BASE_PACKAGE_PATH}"
+if [[ -d "${BASE_PARCEL_PATH}" ]]
+then
+  CLIENT_DIR="${BASE_PARCEL_PATH}/lib/hadoop/client"
+else
+  CLIENT_DIR="${BASE_PACKAGE_PATH}/client"
+fi
+
+# We need an explicit classpath as JNI does not support wildcard classpaths
+for jar_file in $(ls $CLIENT_DIR/*.jar)
 do
-  export CLASSPATH=$CLASSPATH:$file
+  export CLASSPATH=$CLASSPATH:$jar_file
 done
+
 export LD_LIBRARY_PATH="$JAVA_HOME/jre/lib/amd64/server/"
+
+if [[ -d "${BASE_PARCEL_PATH}" ]]
+then
+  export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BASE_PARCEL_PATH/lib/hadoop/lib/native/"
+fi
 
 ./test
