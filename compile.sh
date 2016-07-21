a253145f1b45981607d8059b6e17be0fa868e2cf
Harsh J <harsh@cloudera.com>
2016-07-21 09:03:47 +0530

    Add parcel detection into compile script

--- compile.sh
+++ compile.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
-source /usr/libexec/bigtop-detect-javahome
-export CFLAGS="-O2 -g -Wall -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
+source /usr/bin/bigtop-detect-javahome
+
+BASE_PACKAGE_PATH="/usr/lib/hadoop/"
+BASE_PARCEL_PATH="/opt/cloudera/parcels/CDH/"
+
+if [[ -d "$BASE_PACKAGE_PATH" ]]
+then
+  export LD_LIBRARY_PATH="$BASE_PACKAGE_PATH/lib/native/"
+elif [[ -d "$BASE_PARCEL_PATH" ]]
+then
+  export LD_LIBRARY_PATH="$BASE_PARCEL_PATH/lib/hadoop/lib/native/"
+  export CFLAGS="-I$BASE_PARCEL_PATH/include/"
+fi
+
+export CFLAGS="$CFLAGS -O2 -g -Wall -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
 export LDFLAGS="-lhdfs -L$JAVA_HOME/jre/lib/amd64/server/ -ljvm"
-export LD_LIBRARY_PATH="$JAVA_HOME/jre/lib/amd64/server/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$JAVA_HOME/jre/lib/amd64/server/"
 
 gcc $CFLAGS $LDFLAGS test.c -o test
